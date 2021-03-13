f324a3633303879b02f6fdae08e9d91359625333
stanpov <popovstanislav@abv.bg>
2021-03-13 13:59:19 +0200

    fixed nav css

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navigation.css"
+
+function Navigation() {
+    return (
+        <div className="navigation_div">
+            <nav>
+                <ul className="navigation_ul">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Register</a></li>
+                    <li><a href="#">RegisteruserId</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
