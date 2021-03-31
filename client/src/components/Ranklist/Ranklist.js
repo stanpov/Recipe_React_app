0740f83b7caf59862a461344eb71ca25c2d1bf9a
stanpov <popovstanislav@abv.bg>
2021-03-31 16:23:09 +0300

    fixed Ranklist
    updated ranklist component to be carousel.It is looking better now.

--- client/src/components/Ranklist/Ranklist.js
+++ client/src/components/Ranklist/Ranklist.js
@@ -1,34 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import "./Ranklist.css";
 import * as recipeServices from '../../services/recepiServices/index';
-import RecipeOfRanklist from '../RecipeOfRanklist/RecipeOfRanklist';
+
 
 
 const Ranklist = () => {
     const [recipe, setRecipe] = useState([])
+    const [index,setIndex] = useState(0)
+    
+    
 
     useEffect(() => {
         recipeServices.getRanklist().then(resp => {
-            setRecipe(resp.data)
+            setRecipe(resp.data[index])
         })
-    }, [])
+    },[index])
+
+    const nextRecipe = ()=>{
+        setIndex(prevstate=>prevstate + 1)
+        
+    }
+
+    const prevRecipe = ()=>{
+        setIndex(prevstate=>prevstate - 1)
+    }
 
     return (
         <div className="container-xl">
             <div className="container">
                 <h2 className="top-recipes-header">Top 5 Recipes:</h2>
-                {recipe.slice(0,5).map(r => {
-                    return (
+                    <div className="carousel">
+                        <div
+                        className="carouselInner"
+                        style={{ backgroundImage: `url(${recipe.imageUrl})`,opacity:0.9 }}>
+                            <div className="left">
+                                {index > 0 ?
+                                <button className="carouselLeftBtn" onClick={prevRecipe}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                                </button> : null}
 
-                        <div key={r._id} className="ranklistHolder">
-                            <RecipeOfRanklist recipe={r} />
+                            </div>
+                            <div className="center">
+                                <h1 className="carouselTitle">{recipe.title}</h1>
+                                <span className="carouselLikes">Likes: {recipe.likes ? recipe.likes.length : 0}</span>
+                                <span className ="carouselComments">Comments: {recipe.comments ? recipe.comments.length : 0}</span>
+                            </div>
+                            <div className="right">
+                                {index <= 4 ?
+                                <button className="carouselRightBtn" onClick={nextRecipe}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg> 
+                                </button> : null}
+                            </div>
                         </div>
-                    )
+                    </div>
+             </div>
 
-                })}
-        </div>
-
-            </div>
+         </div>
     )
 }
 
